3468cd28717defbf5b12edaa21a3ba6e066ea88e
MMBobur <maxammadjonov.m.b@gmail.com>
2021-08-23 11:00:32 +0500

    assssdasdad

---
+++ src/pages/home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="navbar"></div>
+            <div className="recentPost"></div>
+            <div className="mostPost">
+                <div className="left"></div>
+                <div className="right"></div>
+            </div>
+            <div className="footer"></div>
+        </div>
+    )
+}
+
+export default Home
